284e03aabb16235c6c9fc62d3fdab8a0582d1c55
alice <alice>
2017-03-06 21:14:15 -0600

    first commit

---
+++ casestudy1.r
@@ -0,0 +1,8 @@
+# Download the files for the case Study
+library(downloader)
+download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", destfile ="GDPData.csv")
+download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", destfile="EducationData.csv")
+
+#Read the GDPData into R and check it out
+GDP<- read.csv("GDPData.csv")
+str(GDP)
