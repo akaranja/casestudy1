735a61ba351d05b4441e62bba1a60da268c3182b
Alice Karanja <akaranja@smu.edu>
2017-03-22 00:37:58 -0500

    sorting and more cleaningg up of Merged dataset.

--- merged GDP+Educ Data.R
+++ merged GDP+Educ Data.R
@@ -6,19 +6,24 @@ View(GDP_Educ)
 # many missing values per column
 colSums(is.na(GDP_Educ))
 
+str(GDP_Educ)
+#convert GDP column fron factor to numeric column
+GDP_Educ$GDP <- as.numeric(gsub("[^[:digit:]]","", GDP_Educ$GDP))
+View(GDP_Educ)
 
-#remove any rows with NAs/missing values in ranking column
-GDP_Educ.clean <- GDP_Educ[!is.na(GDP_Educ$Ranking),]
+
+#remove any rows with NAs/missing values in GDP column
+GDP_Educ.clean <- GDP_Educ[!is.na(GDP_Educ$GDP),]
 View(GDP_Educ.clean)
 nrow(GDP_Educ.clean)
 
 # How many Ids match
-GDP_Educ$match <- match(FGDPData$CountryCode, EducationData.final$CountryCode, nomatch=0)
-nrow(GDP_Educ)
+#GDP_Educ$match <- match(FGDPData$CountryCode, EducationData.final$CountryCode, nomatch=0)
+#nrow(GDP_Educ)
 
 #sort the data frame in ascending order
-GDP_Educ.sort <- GDP_Educ.clean[order(GDP) , ]
-GDP_Educ.sort[1:15]
-
-sort1.hsb2 <- hsb2[order(read) , ]
-sort1.hsb2[1:15, ]
+GDP_Educ.sort <-GDP_Educ.clean[order(GDP_Educ.clean$GDP) , ]
+View(GDP_Educ.sort)
+GDP_Educ.sort[1:15, ]
+#the 13th country from sorted dataframe
+thirteenth <- GDP_Educ.sort$GDP[13]
