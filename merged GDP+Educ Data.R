9badb18730304093977e72cc48f4f608e5222150
Alice Karanja <akaranja@smu.edu>
2017-03-23 01:17:09 -0500

    finishing analysis questions

--- merged GDP+Educ Data.R
+++ merged GDP+Educ Data.R
@@ -3,6 +3,8 @@ GDP_Educ<- merge (FGDPData, EducationData.final, by="CountryCode", all=TRUE)
 library(reshape2)
 library(dplyr)
 library(ggplot2)
+library(plyr)
+library(data.table)
 GDP_Educ
 View(GDP_Educ)
 # how many missing values per column
@@ -31,6 +33,9 @@ GDP_Educ <- GDP_Educ[!is.na(GDP_Educ$CountryCode),]
 View(GDP_Educ)
 nrow(GDP_Educ)
 
+# how many missing values per column
+colSums(is.na(GDP_Educ))
+
 
 #sort the data frame in ascending order
 GDP_Educ.sort <-GDP_Educ.clean1[order(GDP_Educ.clean1$GDP) , ]
@@ -41,6 +46,63 @@ GDP_Educ.sort[1:15, ]
 GDP_Educ.sort [13,5]
 
 
-#average GDPrankings for "High Income:OECD
-mean()
-unique(GDP_Educ.sort$)
+#average GDPrankings for "High Income:nonOECD group
+nonOECD<- subset(GDP_Educ.sort, Income.Group =='High income: nonOECD')
+View(nonOECD)
+mean(nonOECD$GDP, na.rm=TRUE)
+
+
+#average GDPrankings for "High Income:OECD group
+OECD<- subset(GDP_Educ.sort, Income.Group =='High income: OECD')
+View(OECD)
+mean(OECD$GDP, na.rm = TRUE)
+
+#Summary satatistics of GDP by Income Groups
+## How many income groups? =6
+str(GDP_Educ.sort$Income.Group)
+#Summary = (Min, 1st quartile, Median, Mean,3rd Quartile and Max)
+# listed per IncomeGroup
+GDP_Educ.final<- data.table(GDP_Educ.sort)
+setkey(GDP_Educ.final, Income.Group)
+aggre_GDP_Educ <- as.data.frame(GDP_Educ.final[,summary(GDP, na.rm= T), by= Income.Group])
+aggre_GDP_Educ
+
+# how many missing values per column
+colSums(is.na(GDP_Educ.final))
+
+
+#Cut GDP ranking into 5 Quartile Groups
+GDP_Educ.final$quartile <- ntile(GDP_Educ.final$GDP, 5)  
+str(GDP_Educ.final)
+
+#Table of GDP Ranking vs Income Group
+GDP_Educ.table <- with(GDP_Educ.final, table(quartile, Income.Group))
+View(GDP_Educ.table)
+
+# how many missing values per column
+colSums(is.na(GDP_Educ.table))
+
+#How many countries are Lower Middle Income but among the 38 nations with highest GDP
+#Sort by Quartile in Descending order
+#list all Rows with Quartile =5
+GDP_Educ.table[5, ]
+## Lower Middle Income = 4 Countries.
+
+
+#GDP_Educ.table1 <- with(GDP_Educ.sort, table(CountryCode, GDP))
+#View(GDP_Educ.table1)
+
+#ggplot(GDP_Educ.sort, aes(x=GDP)GDP_Educ.sort$CountryCode, main ="GDP vs Country ", xlab="GDP", ylab="CountryCode")
+#geom_histogram(binwidth=.5, colour="black", fill="white") +
+ # geom_vline(aes(xintercept=mean(GDP, na.rm=T)),   # Ignore NA values for mean
+             color="red", linetype="dashed", size=1)
+
+ggplot(GDP_Educ.sort, aes(x=GDP)) +
+  geom_histogram(binwidth=.5, colour="black", fill="white") +
+  geom_vline(aes(xintercept=mean(GDP, na.rm=T)),   # Ignore NA values for mean
+             color="red", linetype="dashed", size=1)
+
+
+
+
+
