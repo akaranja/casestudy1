88ae25675f2deb38298daf155bb4afb26b235f0f
Alice Karanja <akaranja@smu.edu>
2017-03-22 20:30:02 -0500

    more questions on merged data

--- merged GDP+Educ Data.R
+++ merged GDP+Educ Data.R
@@ -1,29 +1,46 @@
 #Merging the data
 GDP_Educ<- merge (FGDPData, EducationData.final, by="CountryCode", all=TRUE)
 library(reshape2)
+library(dplyr)
+library(ggplot2)
 GDP_Educ
 View(GDP_Educ)
-# many missing values per column
+# how many missing values per column
 colSums(is.na(GDP_Educ))
-
 str(GDP_Educ)
+
 #convert GDP column fron factor to numeric column
 GDP_Educ$GDP <- as.numeric(gsub("[^[:digit:]]","", GDP_Educ$GDP))
 View(GDP_Educ)
 
 
-#remove any rows with NAs/missing values in GDP column
+
+#remove any rows with NAs/missing values in GDP grom the GDP data
+#and Long.name column from the Education Data.
+# Ensures that the remaining rows have data from both datasets. 
 GDP_Educ.clean <- GDP_Educ[!is.na(GDP_Educ$GDP),]
 View(GDP_Educ.clean)
 nrow(GDP_Educ.clean)
 
+GDP_Educ.clean1 <- GDP_Educ.clean[!is.na(GDP_Educ.clean$Long.Name),]
+View(GDP_Educ.clean1)
+nrow(GDP_Educ.clean1)
+
 # How many Ids match
-#GDP_Educ$match <- match(FGDPData$CountryCode, EducationData.final$CountryCode, nomatch=0)
-#nrow(GDP_Educ)
+GDP_Educ <- GDP_Educ[!is.na(GDP_Educ$CountryCode),]
+View(GDP_Educ)
+nrow(GDP_Educ)
+
 
 #sort the data frame in ascending order
-GDP_Educ.sort <-GDP_Educ.clean[order(GDP_Educ.clean$GDP) , ]
+GDP_Educ.sort <-GDP_Educ.clean1[order(GDP_Educ.clean1$GDP) , ]
 View(GDP_Educ.sort)
 GDP_Educ.sort[1:15, ]
+
 #the 13th country from sorted dataframe
-thirteenth <- GDP_Educ.sort$GDP[13]
+GDP_Educ.sort [13,5]
+
+
+#average GDPrankings for "High Income:OECD
+mean()
+unique(GDP_Educ.sort$)
